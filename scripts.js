29161ea336d6ac0571f032512f58df1f16e7f38a
alicia3194 <aliciacortinez@gmail.com>
2023-10-03 13:28:48 +0200

    add/dom

--- scripts.js
+++ scripts.js
@@ -1,1 +1,38 @@
 console.log(document.title);
+
+// Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon".
+const toReplace = document.querySelector("#gen-1");
+toReplace.textContent = "Generasión 1 Pokimon";
+
+// Cambia el color de fondo de la primera generación de Pokimon.
+
+const color = document.querySelector(".infocard-list-pkmn-lg");
+color.style.backgroundColor = "#F7ACCF";
+
+// Imprime por consola la URL de la página.
+
+const url1 = document.location.href;
+console.log(url1);
+
+// Imprime por consola el dominio de la página.
+
+const domi = document.location.hostname;
+console.log(domi);
+
+// Imprime todos los nodos de imagen.
+const nodos = document.querySelectorAll(".infocard-lg-img");
+console.log(nodos);
+
+// Sustituye el atributo "src" de todas las
+// imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+
+let imagenes = document.querySelectorAll("img");
+
+for (let i = 0; i < imagenes.length; i++) {
+  imagenes[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+}
+
+// Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itype flying
+
+// const fondo = document.querySelector(".infocard-lg-data text-muted");
+// fondo.style.backgroundColor = "#392759";
